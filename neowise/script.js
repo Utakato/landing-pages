f8f5edd3cc7eb9ea3f640ecffdeee335ac2ab21c
Utakato <copyresearch98@gmail.com>
2022-04-02 18:27:28 +0300

    Add responsive styles + mobile menu

--- neowise/script.js
+++ neowise/script.js
@@ -1,8 +1,7 @@
-const SmoothScroll = element => {element.scrollIntoView({ 
-    behavior: 'smooth' 
-  })}
-
 const menuButtons = document.getElementsByClassName("menu-button")
+const menu = document.getElementsByTagName('nav')[0]
+
+// Menu Scroll
 
 for(i=0;i<menuButtons.length; i++) {
     const button = menuButtons[i] 
@@ -10,6 +9,35 @@ for(i=0;i<menuButtons.length; i++) {
     const section = document.getElementsByClassName(name)
 
     button.addEventListener("click", () => {
-        SmoothScroll(section[0])
+        menu.classList.remove('show')
+        section[0].scrollIntoView({ 
+            behavior: 'smooth' 
+          })
     })
 }
+
+// Menu Mobile
+
+let Responsive = () => {
+    let isMobile = window.innerWidth < 800;
+    if (isMobile){
+        menu.className = 'mobile-menu'
+    } else {
+        menu.className = 'desktop-menu'
+    }
+}
+
+let toggleMenu = () => {
+    const show = 'mobile-menu show'
+    const hide = 'mobile-menu'
+    
+    if (menu.className == hide) {
+        menu.className = show
+    } else {
+        menu.className = hide
+    }
+}
+
+window.addEventListener('resize', () => {Responsive()})
+
+Responsive()
